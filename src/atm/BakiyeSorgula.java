31211d0ce772838ef5f0fe2414cf7461013d526e
Ersin Akun <ersinakun34@gmail.com>
2022-09-09 16:34:21 +0300

    9eyl22

--- src/atm/BakiyeSorgula.java
+++ src/atm/BakiyeSorgula.java
@@ -1,8 +1,10 @@
 package atm;
 
 public class BakiyeSorgula {
+        // baslangicta hesapta 10bin olsun..
      public static int hesaptakiPara = 10000;
 
+     //hesaptaki para goruntulensin ve para yatirma ve para cekmelerde orasi dinamik olarak calissin
      public static int hesaptakiPara(){
          System.out.println("Hesabinizda "+hesaptakiPara+" TL bulunmaktadir");
     return hesaptakiPara;
